73a7e9ea3e3e42964b23c874da65b44c2be060d6
JoeCap <kapt.tesla@gmail.com>
2024-05-05 12:14:20 +0500

    rename gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
 import argparse
 
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', metavar='first_file')
-    parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     args = parser.parse_args()
-    print(args.accumulate(args.first_file))
+    if "-h" in args:
+        parser.print_help()
 
 
 if __name__ == '__main__':
