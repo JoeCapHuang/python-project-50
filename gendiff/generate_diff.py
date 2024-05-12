e5e8ee7d4492f964ee2e37b1ba596ea0b8154da3
JoeCap <kapt.tesla@gmail.com>
2024-05-12 18:09:50 +0500

    add generate_diff.py

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+
+
+def normalize_value(value):
+    if value is False or True:
+        return str(value).lower()
+    else:
+        return value
+
+
+def pars_arg():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    return parser.parse_args()
+
+
+def gen_diff(filepath1, filepath2) -> str:
+    with (
+        open(filepath1) as file1,
+        open(filepath2) as file2,
+    ):
+        file1 = json.load(file1)
+        file2 = json.load(file2)
+        result = '{\n'
+        for key, value in sorted(file1.items()):
+            value = normalize_value(value)
+            if key in file2 and value == file2[key]:
+                result += f'    {key}: {value}\n'
+            elif key not in file2 or value != file2[key]:
+                result += f'  - {key}: {value}\n'
+        for key, value in sorted(file2.items()):
+            value = normalize_value(value)
+            if key not in file1 or value != file1[key]:
+                result += f'  + {key}: {value}\n'
+        result += '}'
+        return result
