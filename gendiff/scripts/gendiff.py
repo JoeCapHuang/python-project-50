dbfde043575fe197d6711228521adf88d9f40cad
JoeCap <kapt.tesla@gmail.com>
2024-05-12 18:09:23 +0500

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,11 @@
-import argparse
+from gendiff.generate_diff import pars_arg
+from gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
+    args = pars_arg()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':
