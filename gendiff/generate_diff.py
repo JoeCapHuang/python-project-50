ed15aa95ba104d0e463f23b2b5caa7e4454c8b8d
JoeCapHuang <kapt.tesla@gmail.com>
2024-07-30 09:12:00 +0300

    update gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -10,7 +10,9 @@ def normalize_value(value):
 
 
 def pars_arg():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
