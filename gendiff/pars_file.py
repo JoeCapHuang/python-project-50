601cf04446f9b75620210ac92ffb6c0b2420089f
JoeCapHuang <kapt.tesla@gmail.com>
2024-08-22 19:39:40 +0300

    update open_and_parse_file

--- gendiff/pars_file.py
+++ gendiff/pars_file.py
@@ -4,12 +4,13 @@ import os
 
 
 def open_and_parse_file(filepath):
-    file, file_extension = open(filepath), os.path.splitext(filepath)[-1].lower()
+    with open(filepath) as file:
+        file_extension =  os.path.splitext(filepath)[-1].lower()
 
-    match file_extension:
-        case '.json':
-            return json.load(file)
-        case '.yaml' | '.yml':
-            return yaml.safe_load(file)
-        case _:
-            print('invalid file extension')
+        match file_extension:
+            case '.json':
+                return json.load(file)
+            case '.yaml' | '.yml':
+                return yaml.safe_load(file)
+            case _:
+                print('invalid file extension')
